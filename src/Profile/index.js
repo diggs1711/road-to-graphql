567f7e0ed9adae1ccadcbd3ea7903a336557033c
apple <davidhiggins1712@gmail.com>
2019-08-11 17:39:37 +1000

    m

--- src/Profile/index.js
+++ src/Profile/index.js
@@ -1,28 +1,36 @@
 import React from 'react';
 import { useQuery } from '@apollo/react-hooks';
 import gql from 'graphql-tag';
+import Loading from '../Loading';
+import RepositoryList, { REPOSITORY_FRAGMENT } from '../Repository';
+import ErrorMessage from '../Error';
 
-const GET_CURRENT_USER = gql`
+const GET_REPOSITORIES_OF_CURRENT_USER = gql`
 	{
 		viewer {
 			login
 			name
+			repositories(first: 5, orderBy: { direction: DESC, field: STARGAZERS }) {
+				edges {
+					node {
+						...repository
+					}
+				}
+			}
 		}
 	}
+
+    ${REPOSITORY_FRAGMENT}
 `;
 
 const Profile = () => {
-	const { loading, data, error } = useQuery(GET_CURRENT_USER);
-	if (loading) return <p>Loading...</p>;
-	if (error) return <p>Error :(</p>;
+	const { loading, data, error } = useQuery(GET_REPOSITORIES_OF_CURRENT_USER);
+	if (error) return <ErrorMessage error={error} />;
 
 	const { viewer } = data;
+	if (loading || !viewer) return <Loading />;
 
-	return (
-		<div>
-			{viewer.name} {viewer.login}
-		</div>
-	);
+	return <RepositoryList repository={viewer.repositories} />;
 };
 
 export default Profile;
--- src/index.js
+++ src/index.js
@@ -2,26 +2,44 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { ApolloClient } from 'apollo-client';
 import { ApolloProvider } from 'react-apollo';
+import { RetryLink } from 'apollo-link-retry';
 import { HttpLink } from 'apollo-link-http';
+import { onError } from 'apollo-link-error';
 import { InMemoryCache } from 'apollo-cache-inmemory';
-
 import './index.css';
 import App from './App/index';
 import * as serviceWorker from './serviceWorker';
+import { ApolloLink } from 'apollo-link';
 
-const GITHUB_BASE_URL = 'http://api.github.com/graphql';
-console.log(process.env);
+const GITHUB_BASE_URL = 'https://api.github.com/graphql';
+
 const httpLink = new HttpLink({
 	uri: GITHUB_BASE_URL,
 	headers: {
-		authorization: `Bearer ${process.env.REACT_APP_GITHUB_PERSONAL_ACCESS_TOKEN}`
+		Authorization: `bearer ${process.env.REACT_APP_GITHUB_PERSONAL_ACCESS_TOKEN}`
 	}
 });
 
 const cache = new InMemoryCache();
+const retryLink = new RetryLink();
+const errorLink = onError(({ graphQLErrors, networkError }) => {
+	if (graphQLErrors) {
+		graphQLErrors.map(({ message, locations, path }) =>
+			console.log(
+				`[GraphQL Error]: Message:${message}, Location: ${locations}, Path: ${path}`
+			)
+		);
+	}
+
+	if (networkError) {
+		console.log(`[Network Error]: ${networkError}`);
+	}
+});
+
+const link = ApolloLink.from([ errorLink, httpLink, retryLink ]);
 
 const client = new ApolloClient({
-	link: httpLink,
+	link,
 	cache
 });
 
